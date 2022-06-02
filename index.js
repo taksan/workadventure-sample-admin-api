b40e60db3792fd7a3794e93299a4d71ff2bb9c9a
Takeuchi <g.takeuchi@gmail.com>
2022-06-02 08:35:19 -0300

    Fixed example to correctly document how the `/api/room/access` endpoint should behave

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express           = require("express")
 const app               = express()
-const wokaList          = require('./woka.json')
-const availableWokas    = require('./availableWorks.json')
+const wokas          = require('./woka.json')
 
+function getAllTextures() {
+    let wokas = []
+    Object.keys(wokaList).forEach(groupName => {
+        let group = wokaList[groupName]
+        group.collections.forEach(collection => {
+            collection.textures.forEach(texture => {
+                wokas.push({
+                    id: texture.id,
+                    url: texture.url,
+                    layer: groupName
+                })
+            })
+        })
+    })
+    return wokas
+}
 
 app.get("/api/map", (req, res) => {
     console.debug("Receive map request with parameters:", req.query)
@@ -10,35 +25,42 @@ app.get("/api/map", (req, res) => {
     res.send(JSON.stringify({
         mapUrl : "http://maps.localtest.me/starter/map.json",
         group : "wa",
-        authenticationMandatory: true
+        authenticationMandatory: false
     }
     ))
 })
 
 app.get("/api/woka/list", (req, res) => {
-    res.send(wokaList)
+    // You receive the userId 
+    res.send(wokas)
 })
 
 app.get("/api/room/access", (req, res) => {
-    console.debug(req.query)
-    res.send(JSON.stringify(
+    let characterLayers = req.query.characterLayers || []
+
+    // Notice that we filter the textures based on the user selection (given on characterLayers)
+    let textures = getAllTextures().filter(woka => characterLayers.indexOf(woka.id) !== -1)
+
+    // make sure to preserve the texture order (given on characterLayers)
+    textures.sort( (t1, t2) => characterLayers.indexOf(t1.id) - characterLayers.indexOf(t2.id) )
+    res.send(
         {
-            email: "foo@good",
+            email: "someuser@iamokay",
             userUuid: req.query.userIdentifier,
             tags: ["user"],
-            visitCardUrl: null,
-            textures: availableWokas,
+            visitCardUrl: `http://alink-to-the-user-profile.cool?user=${req.query.userIdentifier}`,
+            textures: textures,
             messages:[],
             anonymous: false
         }
-    ))
+    )
 })
 
 
 app.get("*", (req, res) => {
+    console.debug("Request not handled:", req.url)
     console.debug(req.method)
-    console.debug(req.url)
-    console.debug(req.body)
+    console.debug(req.query)
     res.send("{}")
 })
 
