cdddc9018ea2b30fb99d94b2fd229bb3ab9b8d6b
Takeuchi <g.takeuchi@gmail.com>
2022-11-08 22:30:10 -0300

    Adds support for proposed companion and completes example with custom woka

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express           = require("express")
-const app               = express()
-const wokas          = require('./woka.json')
+const express  = require("express")
+const cors = require('cors')
+const app      = express()
+const wokaList   = require('./woka.json')
+const companions = require("./companions.json")
 
 function getAllTextures() {
     let wokas = []
@@ -19,20 +21,36 @@ function getAllTextures() {
     return wokas
 }
 
+app.use(cors())
+
+app.use(express.static('public'))
+
+app.get("/api/capabilities", (req, res) => {
+    res.send({
+        "api/companion/list": "v1"
+    })
+})
+
+app.get("/api/companion/list", (req, res) => {
+    res.send(companions)
+})
+
 app.get("/api/map", (req, res) => {
     console.debug("Receive map request with parameters:", req.query)
 
     res.send(JSON.stringify({
-        mapUrl : "http://maps.localtest.me/starter/map.json",
+        mapUrl : "https://taksan.github.io/workadventure-map-starter-kit/map.json",
         group : "wa",
-        authenticationMandatory: false
+        authenticationMandatory: false,
+        redirectUrl: null,
+        mucRooms: null
     }
     ))
 })
 
 app.get("/api/woka/list", (req, res) => {
     // You receive the userId 
-    res.send(wokas)
+    res.send(wokaList)
 })
 
 app.get("/api/room/access", (req, res) => {
@@ -51,7 +69,8 @@ app.get("/api/room/access", (req, res) => {
             visitCardUrl: `http://alink-to-the-user-profile.cool?user=${req.query.userIdentifier}`,
             textures: textures,
             messages:[],
-            anonymous: false
+            anonymous: false,
+            mucRooms: [{ name: "Connected Users", type: "default", "url": req.query.playUri }]
         }
     )
 })
@@ -61,7 +80,56 @@ app.get("*", (req, res) => {
     console.debug("Request not handled:", req.url)
     console.debug(req.method)
     console.debug(req.query)
+
     res.send("{}")
 })
 
-app.listen(8080)
+const ADMIN_URL = process.env.ADMIN_URL
+
+app.listen(8080, ()=> {
+
+    companions.push({
+        name: "extra",
+        "textures": [
+            {
+                "name": "blauhai3.png",
+                "img": `${ADMIN_URL}/companions/blauhai3.png`
+            },
+            {
+                "name": "Smiley.png",
+                "img": `${ADMIN_URL}/companions/Smiley.png`
+            }
+        ]
+    })
+    wokaList["woka"]["collections"].push({
+        "name": "extra",
+        "position": 1,
+        "textures": [
+            {
+                "id": "pipo-nekonin001",
+                "name": "pipo-nekonin001",
+                "url": `${ADMIN_URL}/wokas/pipo-nekonin001.png`,
+                "position": 0
+            }
+        ]
+    })
+
+    wokaList["hair"]["collections"].push({
+        "name": "extra",
+        "position": 1,
+        "textures": [
+            {
+                "id": "samurai-dark",
+                "name": "samurai-dark",
+                "url": `${ADMIN_URL}/hair/_samurai_dark.png`,
+                "position": 0
+            },
+            {
+                "id": "samurai-white",
+                "name": "samurai-white",
+                "url": `${ADMIN_URL}/hair/_samurai_white.png`,
+                "position": 1
+            }
+        ]
+    })
+})
